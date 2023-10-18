fdc02dd9dbb21ae856d300ca81da8e525b2c24fa
Abishek <abishek@happyfoxs-MacBook-Pro-2.local>
2023-10-18 13:26:40 +0530

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/serviceworker.js')
+  navigator.serviceWorker.register('/sw.js')
     .then(reg => {
         console.log('service worker registered', reg)
         navigator.serviceWorker.onmessage = (evt) => {
@@ -16,4 +16,17 @@ if('serviceWorker' in navigator){
         }
     })
     .catch(err => console.log('service worker not registered', err));
+}
+
+const url = new URL(window.location.href);
+
+// Get the value of the 'title' query parameter
+const title = url.searchParams.get('title');
+// Check if 'title' exists and display it
+const ele = document.getElementById('txt');
+ele.innerText = title;
+if (title) {
+  console.log("Value of 'title' query parameter:", title);
+} else {
+  console.log("'title' query parameter not found in the URL.");
 }
---
+++ sw.js
@@ -0,0 +1,36 @@
+// function handleFileShare(evt) {
+//     try {
+//       evt.responseWith(Response.redirect("./"))
+// 
+//       evt.waitUntil(async function() {
+//           const data = await evt.request.formData();
+//           const client = await self.clients.get(evt.resultingClientId);
+//           const file = data.get('file');
+//   
+//           client.postMessage({ file });
+//       })
+//     } catch {}
+// }
+
+self.addEventListener('install', evt => {
+  console.log('service worker installed');
+});
+
+self.addEventListener("activate", (event) => {});
+self.addEventListener('fetch', evt => {
+  // const url = new URL(evt.request.url);
+
+  if (evt.request.method === 'POST') {
+    // handleFileShare(evt);
+    // self.clients.get(evt.resultingClientId).then((client) => {
+    //   client.postMessage({ file: 'Hello 111' });
+    // })
+    evt.responseWith((async () => {
+      const formdata = await evt.request.formData();
+      // const data = [...formdata.entries()]
+      const title = formdata.get('title') || 'One';
+      const responseUrl = `/?title=${title}`
+      return Response.redirect(responseUrl, 303);
+    })())
+  }
+});
